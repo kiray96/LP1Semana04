cdae5496fce1f7bd53ff2697e7cbba455eabed29
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-10 23:37:58 +0000

    Check if there are arguments on the program and if not run the new version of Decor

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -14,14 +14,21 @@ namespace BetterDecorador
         /// <param name="args">Console Line Arguments</param>
         private static void Main(string[] args)
         {
-            // Converter argumentos da linha de comando em variaveis utilizáveis
-            string s = args[0];
-            char c = char.Parse(args[1]);
-            int i = int.Parse(args[2]);
+            if (args.GetLength(0) != 0)
+            {
+                // Converter argumentos da linha de comando em variaveis utilizáveis
+                string s = args[0];
+                char c = char.Parse(args[1]);
+                int i = int.Parse(args[2]);
 
-            // Imprimir a string construida pelo Decor
-            Console.WriteLine(Decor(s, c, i));
-            
+                // Imprimir a string construida pelo Decor
+                Console.WriteLine(Decor(s, c, i));
+            }
+            else
+            {
+                Console.WriteLine(Decor());
+            }
+
         }
 
 
@@ -34,13 +41,13 @@ namespace BetterDecorador
         /// <param name="times">Number of decorators that will be added before
         ///  and after the original string</param>
         /// <returns>Decorated string</returns>
-        private static string Decor(string s , char dec, int times)
+        private static string Decor(string s, char dec, int times)
         {
             // Iniciar o result com a string s e um espaço de cada lado
             string result = $" {s} ";
 
             // Repetir o número de vezes pedida 
-            for(int i = 0; i < times; i++)
+            for (int i = 0; i < times; i++)
             {
                 // Acrescentar char dec antes e depois do que já existe dentro 
                 // do result
@@ -51,7 +58,7 @@ namespace BetterDecorador
             return result;
         }
 
-        private static string Decor() => Decor("User did not specify args !", '=', 3);
-        
+        private static string Decor() => Decor("User did not specify args!", '=', 3);
+
     }
 }
