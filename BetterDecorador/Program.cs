c3c251854bf6b5b5ec251f3846786f5854099c74
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-10 22:53:42 +0000

    Creat BetterDecorador project and complete the exercises

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s = args[0];
+            char c = char.Parse(args[1]);
+            int i = int.Parse(args[2]);
+
+            Console.WriteLine(Decor(s, c, i));
+            
+        }
+
+        private static string Decor(string s , char dec, int times)
+        {
+            string result = $" {s} ";
+
+            for(int i = 0; i < times; i++)
+            {
+                result = $"{dec}{result}{dec}";
+            }
+
+            return result;
+        }
+    }
+}
