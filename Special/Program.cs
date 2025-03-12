71fa3a536c753d397aaa561d779a6865de207836
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-12 22:16:58 +0000

    Make the Special exercise

---
+++ Special/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.XPath;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
+
+        }
+
+        private static int GetSpecial(int n)
+        {
+            if (n <= 1) 
+            {
+                return n;
+            }
+
+            else
+            {
+                int result = GetSpecial(n-1) + 2 * GetSpecial(n-2);
+                return result;
+            }
+            
+        }
+    }
+}
